ce390dabce0124bde4d4f2274aa684f6f89caca8
Green Lightning <GreenLightning.git@googlemail.com>
2022-01-06 19:41:41 +0100

    Refactor code

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 	"strings"
 )
 
+type Hints struct {
+	Base string
+	Good string
+	Bad  string
+}
+
 const (
 	sourceDir   = "dicts"
 	filteredDir = "filtered"
@@ -26,63 +32,8 @@ func main() {
 		filter()
 	}
 
-	if flag.NArg() == 0 {
-		return
-	}
-
-	words := readLines(filepath.Join(filteredDir, *dictFlag))
-	argRegex := regexp.MustCompile(`^([_A-Za-z]{5})(?:\+([A-Za-z]+))?(?:-([A-Za-z]+))?$`)
-
-	for i, arg := range flag.Args() {
-		matches := argRegex.FindStringSubmatch(arg)
-		if matches == nil {
-			fmt.Printf("invalid argument %q\n", arg)
-			continue
-		}
-
-		base := strings.ToUpper(matches[1])
-		good := strings.ToUpper(matches[2])
-		bad := strings.ToUpper(matches[3])
-
-	loop:
-		for _, word := range words {
-			if len(word) != len(base) {
-				continue
-			}
-
-			for i := 0; i < len(base); i++ {
-				if base[i] != '_' && word[i] != base[i] {
-					continue loop
-				}
-			}
-
-			for i := 0; i < len(good); i++ {
-				found := false
-				for j := 0; j < len(word); j++ {
-					if word[j] == good[i] {
-						found = true
-						break
-					}
-				}
-				if !found {
-					continue loop
-				}
-			}
-
-			for i := 0; i < len(bad); i++ {
-				for j := 0; j < len(word); j++ {
-					if word[j] == bad[i] {
-						continue loop
-					}
-				}
-			}
-
-			fmt.Println(word)
-		}
-
-		if i+1 < flag.NArg() {
-			fmt.Println(strings.Repeat("=", 20))
-		}
+	if flag.NArg() != 0 {
+		lookup(flag.Args())
 	}
 }
 
@@ -128,6 +79,70 @@ func filter() {
 
 		writeLines(filepath.Join(filteredDir, name), words)
 	}
+}
+
+func lookup(args []string) {
+	words := readLines(filepath.Join(filteredDir, *dictFlag))
+	argRegex := regexp.MustCompile(`^([_A-Za-z]{5})(?:\+([A-Za-z]+))?(?:-([A-Za-z]+))?$`)
+
+	for i, arg := range args {
+		matches := argRegex.FindStringSubmatch(arg)
+		if matches == nil {
+			fmt.Printf("invalid argument %q\n", arg)
+			continue
+		}
+
+		hints := Hints{
+			Base: strings.ToUpper(matches[1]),
+			Good: strings.ToUpper(matches[2]),
+			Bad:  strings.ToUpper(matches[3]),
+		}
+
+		for _, word := range words {
+			if matchesHints(word, hints) {
+				fmt.Println(word)
+			}
+		}
+
+		if i+1 < flag.NArg() {
+			fmt.Println(strings.Repeat("=", 20))
+		}
+	}
+}
+
+func matchesHints(word string, hints Hints) bool {
+	if len(word) != len(hints.Base) {
+		return false
+	}
+
+	for i := 0; i < len(hints.Base); i++ {
+		if hints.Base[i] != '_' && word[i] != hints.Base[i] {
+			return false
+		}
+	}
+
+	for i := 0; i < len(hints.Good); i++ {
+		found := false
+		for j := 0; j < len(word); j++ {
+			if word[j] == hints.Good[i] {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+
+	for i := 0; i < len(hints.Bad); i++ {
+		for j := 0; j < len(word); j++ {
+			if word[j] == hints.Bad[i] {
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
 func readLines(filename string) []string {
